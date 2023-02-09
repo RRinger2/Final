ffd4f9622a25eb6e7b0709edad34164010e3ff41
RRinger2 <123273515+RRinger2@users.noreply.github.com>
2023-02-08 19:44:06 -0500

    Final 1

---
+++ Final.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+import tkinter as tk
+from tkinter import messagebox
+import openpyxl ,xlrd
+from openpyxl import Workbook
+import pathlib
+
+root=Tk()
+root.title("Customer Data Entry")
+root.geometry('700x400+300+200')
+root.resizable(False,False)
+
+root.mainloop()
